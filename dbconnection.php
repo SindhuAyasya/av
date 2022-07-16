f6643b63e80700f73e8af712ab0ad1babae19fbc
SindhuAyasya <109264852+SindhuAyasya@users.noreply.github.com>
2022-07-15 23:35:03 -0700

    Add files via upload

--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "mca");
+$con=mysqli_connect("localhost", "root", "", "av");
 if(mysqli_connect_errno())
 {
 echo "Connection Fail".mysqli_connect_error();
